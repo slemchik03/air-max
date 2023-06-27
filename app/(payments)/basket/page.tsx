2437a6e8f4603266f403fdf4c36eca174edd3808
slemchik_03 <vadimmaster228@gmail.com>
2023-06-27 23:18:25 +0300

    Add delete from basket dialog

--- app/(payments)/basket/page.tsx
+++ app/(payments)/basket/page.tsx
@@ -1,14 +1,12 @@
 import Basket from "@/components/BasketPage/Basket/Basket";
-import getBasketItems from "@/utils/server/get/getBasketItems";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
   const user = await currentUser();
-  const basketItems = await getBasketItems(user?.id + "");
 
   if (user) {
-    return <Basket basketItems={basketItems} />;
+    return <Basket userId={user.id} />;
   }
   redirect("/sign-in");
 }
